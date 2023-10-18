2af02fe0ec6f17bcc6f608459fb25e41f2139978
Anurag Pramanik <p.anurag@outlook.com>
2023-10-18 09:32:37 +0530

    add codes

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,15 @@
-function findMaxNumber() {}
+function findMaxNumber(num) {
+  // let largestNumber = 0;
+
+  // for (let i = 0; i <= num.length; i++) {
+  //   if (num[i] > largestNumber) {
+  //     largestNumber = num[i];
+  //   }
+  // }
+
+  // return largestNumber;
+
+  return Math.max(...num);
+}
 
 module.exports = findMaxNumber;
