d09a164d1b6f985482d6693ad7f8d1a359779a25
Anurag Pramanik <p.anurag@outlook.com>
2023-10-20 11:08:23 +0530

    update watch script

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,21 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let newArr = [];
+
+  // for (const num of arr1) {
+  //   if (arr2.includes(num)) {
+  //     newArr.push(num);
+  //   }
+  // }
+
+  const set1 = new Set(arr1);
+
+  for (const num of arr2) {
+    if (set1.has(num)) {
+      newArr.push(num);
+    }
+  }
+
+  return newArr;
+}
 
 module.exports = arrayIntersection;
