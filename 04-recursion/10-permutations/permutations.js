9224acd4486776e7ed742355db7ba1a8d5c56361
Anurag Pramanik <p.anurag@outlook.com>
2024-01-21 10:39:45 +0530

    Add code for permutation of string

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,22 @@
-function permutations() {}
+function permutations(str) {
+  const result = [];
+
+  if (str.length === 0) {
+    result.push("");
+    return result;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str[i];
+    const restOfTheString = str.slice(0, i) + str.slice(i + 1);
+    const subPerm = permutations(restOfTheString);
+
+    for (let j = 0; j < subPerm.length; j++) {
+      result.push(firstChar + subPerm[j]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = permutations;
