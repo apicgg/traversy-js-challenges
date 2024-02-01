221574751014900c35b7c0493a9d63263c7bdb3c
Anurag Pramanik <p.anurag@outlook.com>
2024-02-01 10:16:11 +0530

    Add sum of even squares with HOF

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,7 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  const squareOfEvenNumbers = evenNumbers.map((num) => num ** 2);
+  return squareOfEvenNumbers.reduce((sum, square) => sum + square, 0);
+}
 
 module.exports = sumOfEvenSquares;
