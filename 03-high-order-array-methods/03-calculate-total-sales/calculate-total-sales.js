08e519afb7cecdd7ee5507b25bc1ac6911771c42
Anurag Pramanik <p.anurag@outlook.com>
2024-02-04 10:08:16 +0530

    Add code for total sales with tax

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,11 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const total = products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+
+  const tax = (total * taxRate) / 100;
+  return tax + total;
+}
 
 module.exports = calculateTotalSalesWithTax;
