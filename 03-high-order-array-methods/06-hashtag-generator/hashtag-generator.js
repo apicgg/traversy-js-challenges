bfcdb8cc01e708f02031207049d7ae6688159786
Anurag Pramanik <p.anurag@outlook.com>
2024-02-15 22:15:08 +0530

    Add code for generateHashtag

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,18 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.length === 0) return false;
+
+  if (str.length > 140) return false;
+
+  const string = str
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join("");
+
+  const hashTag = "#" + string;
+
+  return hashTag;
+}
 
 module.exports = generateHashtag;
