fdfca847652528b38ae0e6a6542b623c411826d4
Anurag Pramanik <p.anurag@outlook.com>
2024-02-16 08:24:19 +0530

    Add code for isValidIPv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,12 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ip) => {
+  const ipArr = ip.split(".");
+
+  if (ipArr.length !== 4) return false;
+
+  return ipArr.every((bit) => {
+    const num = parseInt(bit);
+    return num >= 0 && num <= 255 && bit === num.toString();
+  });
+};
 
 module.exports = isValidIPv4;
