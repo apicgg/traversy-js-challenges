434832ca9f81591abacc2a9788b0ef62deb30640
Anurag Pramanik <p.anurag@outlook.com>
2024-02-17 08:15:50 +0530

    Add code for analyzeCarMileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,24 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((sum, car) => {
+    return sum + car.mileage;
+  }, 0);
+
+  const averageMileage = totalMileage / cars.length;
+
+  const highestMileageCar = cars.reduce((highest, car) => {
+    return car.mileage > highest.mileage ? car : highest;
+  }, cars[0]);
+
+  const lowestMileageCar = cars.reduce((lowest, car) => {
+    return car.mileage < lowest.mileage ? car : lowest;
+  }, cars[0]);
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
