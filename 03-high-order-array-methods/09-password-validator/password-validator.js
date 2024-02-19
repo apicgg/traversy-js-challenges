e081db113b0d21f39a7f192c88da100c5391a4c0
Anurag Pramanik <p.anurag@outlook.com>
2024-02-19 19:05:51 +0530

    Add code for validatePassword

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,19 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const passwordArr = password.split("");
+
+  const isLengthValid = password.length >= 8;
+
+  const hasUppercase = passwordArr.some((char) => char === char.toUpperCase());
+
+  const hasLowercase = password
+    .split("")
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+  const hasDigit = password
+    .split("")
+    .some((char) => !isNaN(parseInt(char, 10)));
+
+  return isLengthValid && hasUppercase && hasLowercase && hasDigit;
+}
 
 module.exports = validatePassword;
